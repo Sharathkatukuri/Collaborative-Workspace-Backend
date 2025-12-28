c6d3e42534bd3886a1f8523db8d761d0a0a3bfb0
Sharathkatukuri <158730602+Sharathkatukuri@users.noreply.github.com>
2025-12-28 09:45:21 +0530

    Remove commented import and export statements

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-// import mongoose from "mongoose";
 const mongoose  =require("mongoose");
 
 const connectDB = async () => {
@@ -11,4 +10,4 @@ const connectDB = async () => {
   }
 };
 module.exports = connectDB;
-// export default connectDB;
+
