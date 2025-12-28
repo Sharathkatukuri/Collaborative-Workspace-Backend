d0bb1e01dddcad4bfc265482942251bcc767e098
Sharathkatukuri <158730602+Sharathkatukuri@users.noreply.github.com>
2025-12-28 09:45:44 +0530

    Update redis.js

--- config/redis.js
+++ config/redis.js
@@ -14,25 +14,3 @@ redis.on("error", (err) => console.error("Redis error", err));
 module.exports = redis;
 
 
-// const { createClient } = require("redis");
-
-// const redis = createClient({
-//   url: process.env.REDIS_URL,
-//   socket: {
-//     tls: true,
-//     reconnectStrategy: (retries) => {
-//       if (retries > 5) return new Error("Redis retry limit reached");
-//       return Math.min(retries * 200, 1000);
-//     }
-//   }
-// });
-
-// redis.on("connect", () => console.log("Redis connected"));
-// redis.on("error", (err) => console.error("Redis error", err));
-
-// (async () => {
-//    await redis.connect();
-// })();
-
-// module.exports = redis;
-
